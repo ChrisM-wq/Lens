c01deb08fbedf04e43a9d758f9dc89cfcbefb7f0
Chris McHardy <chrismchardy1@gmail.com>
2024-01-09 13:51:32 +0200

    Style preview

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -4,19 +4,20 @@ import 'react-quill/dist/quill.snow.css';
 
 const modules = {
   toolbar: [
-  // ['bold', 'italic', 'underline', 'strike'],
   [{ 'header': [1, 2, false] }],
   ['blockquote', 'code-block'],
   [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-  
+  ['bold', 'italic', 'underline'],
+  [{ align: '' }, { align: 'center' }],
+  ['link', 'image']
 ]};
 
 export default function TextEditor({value, setValue}) {
-  return <ReactQuill 
-  theme="snow" 
-  value={value} 
-  onChange={setValue}
-  modules={modules} 
-  style={{ width: '100%'}}
+  return <ReactQuill
+    theme="snow"
+    value={value}
+    onChange={setValue}
+    modules={modules}
+    style={{ width: '100%'}}
   />;
 }
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import TextEditor from "../components/TextEditor";
 import React, { useState } from "react";
 import parse from 'html-react-parser';
@@ -12,12 +12,16 @@ const CreatePost = () => {
       <Box sx={{ width: '1000px' }}>
         <TextEditor value={value} setValue={setValue} />
       </Box>
-      <Box sx={{ width: '1000px' }}>
+      <Box id="test" sx={{ width: '800px' }}>
         {parse(`${value}`)}
       </Box>
       <div>
         {value}
       </div>
+
+      <Box>
+        <Button variant="pinkBtn">Publish</Button>
+      </Box>
     </Box>
   );
 };
